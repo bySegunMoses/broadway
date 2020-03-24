4d0e5555a2f39c826a9651d0212d7d62962b6fbf
by.SegunMoses <by.segunmoses@gmail.com>
2020-03-24 16:23:10 +0100

    Updates

---
+++ precache-manifest.44516d2212d01122396ab6ca78c5a26d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b21ff082df2c704fbbe82bd440197db",
+    "url": "/broadway/index.html"
+  },
+  {
+    "revision": "76af605f8fa31f616283",
+    "url": "/broadway/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "61d6ff41e3601c02f28e",
+    "url": "/broadway/static/js/2.06f72514.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/broadway/static/js/2.06f72514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76af605f8fa31f616283",
+    "url": "/broadway/static/js/main.97d3b422.chunk.js"
+  },
+  {
+    "revision": "9b7ec6d8ea9ecfd44f23",
+    "url": "/broadway/static/js/runtime-main.804dd07f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/broadway/static/media/logo.5d5d9eef.svg"
+  }
+]);
